e6f21d70389fe22e77525b409fbb70c13353bb11
Tobias Nobile <tobias.nobile@telecomnancy.eu>
2026-02-23 18:05:43 +0100

    Add random quotes generation : each fun quote has its own probability to appear on the main page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify, session
 from back.generate import generate_answer
+import random
 
 bp = Blueprint("chat", __name__)
 
@@ -42,3 +43,23 @@ def history():
 def clear_history():
     session.pop("history", None)
     return jsonify({"message": "Historique effacé"})
+
+CITATIONS = [
+    ("Qu'avez-vous à dire pour votre défense ?", 10),
+    ("Envie de jiguer, pas vous ?", 15),
+    ("En date avec Crazy François", 15),
+    ("* en train de barboter dans l'évier cancéreux du bar *", 20),
+    ("on vient de me barouder aled", 7),
+    ("ici ça bz", 5),
+    ("Je ne suis pas un projet de TNS (mdr)", 5),
+    ("on m'a forcé à prendre du thé", 5),
+    ("nique le cheval whatsapp", 15),
+    ("after chez camille", 5),
+    ("Prompt injection et tu vas repartir mal mon compaing", 7)
+]
+
+@bp.route("/quote", methods=["GET"])
+def quote():
+    quotes = [c[0] for c in CITATIONS]
+    weights = [c[1] for c in CITATIONS]
+    return jsonify({"quote": random.choices(quotes, weights=weights, k=1)[0]})
